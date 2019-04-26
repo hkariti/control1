93e8d3743dc8445a7d62f679b8a474edfbcf5921
Hagai Kariti <hkariti@gmail.com>
2019-04-26 15:55:51 +0300

    Add sine response

--- hw2/CartParams.m
+++ hw2/CartParams.m
@@ -15,3 +15,13 @@ xlabel('Time [s]');
 ylabel('Angle [rad]');
 title('Linear and non-linear response to step input');
 legend('input', 'linear out (\phi)', 'non-linear out (\theta)');
+%% plot output to sine response
+figure;
+hold on;
+plot(linear_in.Time, linear_in.Data);
+plot(linear_out.Time, linear_out.Data);
+plot(nl_out.Time, nl_out.Data);
+xlabel('Time [s]');
+ylabel('Angle [rad]');
+title('Linear and non-linear response to sine input (A=0.1, \omega=1)');
+legend('input', 'linear out (\phi)', 'non-linear out (\theta)');
