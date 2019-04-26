dd31b72c264c80cc3e6012094cf04fc74c3570e9
Hagai Kariti <hkariti@gmail.com>
2019-04-26 15:39:35 +0300

    plot step response

--- hw2/CartParams.m
+++ hw2/CartParams.m
@@ -5,3 +5,13 @@ b = 0.1;
 l = 0.3;
 I = 0.006;
 g= 10;
+%% plot output to step input
+figure;
+hold on;
+plot(linear_in.Time, linear_in.Data);
+plot(linear_out.Time, linear_out.Data);
+plot(nl_out.Time, nl_out.Data);
+xlabel('Time [s]');
+ylabel('Angle [rad]');
+title('Linear and non-linear response to step input');
+legend('input', 'linear out (\phi)', 'non-linear out (\theta)');
